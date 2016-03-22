ca383d54994cf77cfce723c23959888706d3e195
Fatima Khalilova <fatimakhalilova@gmail.com>
2016-03-22 14:50:23 +0100

    in the hello-world.js created a custom html tag helloWorld using the .directive method of the module

---
+++ scripts/hello-world.js
@@ -0,0 +1,6 @@
+angular.module("todoListApp") //do not add and empty array as the 2nd param, since we do not want to create a new module, instead call the already existing one
+.directive("helloWorld", function(){  //attached to the existing module a new directive; 1st param - name, 2nd - call back function
+	return{   //returns an Object which is our directive
+		template: "This is the Hello World Directive" //now this helloWorld can be used as a tag in our html
+	};
+});
