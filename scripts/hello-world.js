5f9be40e7144ddf27d1e57fc6fa4002295929e2c
Fatima Khalilova <fatimakhalilova@gmail.com>
2016-03-22 14:57:39 +0100

    added the restrict option to the directive to be restricted to just an Element

--- scripts/hello-world.js
+++ scripts/hello-world.js
@@ -1,6 +1,7 @@
 angular.module("todoListApp") //do not add and empty array as the 2nd param, since we do not want to create a new module, instead call the already existing one
 .directive("helloWorld", function(){  //attached to the existing module a new directive; 1st param - name, 2nd - call back function
 	return{   //returns an Object which is our directive
-		template: "This is the Hello World Directive" //now this helloWorld can be used as a tag/attr in our html
+		template: "This is the Hello World Directive", //now this helloWorld can be used as a tag/attr in our html
+		restrict: "E"   // can be used to restrict the directive to be used as an element or as an attr of the elements; E - means just element
 	};
 });
