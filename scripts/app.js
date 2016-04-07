93a0acd30cc4089309ebf93a328f3be5562fa970
Fatima Khalilova <fatimakhalilova@gmail.com>
2016-04-07 18:42:49 +0200

    created a service and a dependancy fot the Main Ctrl

--- scripts/app.js
+++ scripts/app.js
@@ -1,5 +1,7 @@
 angular.module("todoListApp", [])
-.controller("mainCtrl", function($scope){ //scope - area of work of the controller
+.controller("mainCtrl", function($scope, dataService){ //scope - area of work of the controller
+	//adding dataService as a param we create the dependancy as needed in order for the service to be called
+	$scope.helloConsole = dataService.helloConsole;
 	$scope.helloWorld = function(){
 		console.log("Hello there!!!!! I'm the Main Ctrl")
 	};
@@ -10,4 +12,9 @@ angular.module("todoListApp", [])
 		{"name" : "enjoy the sun"},
 		{"name" : "enjoy the sun again while it's not raininf"}
 	]
+
+
+})
+.service('dataService', function(){
+	this.helloConsole = function(){ console.log("Hello Console this is Setvice")}
 });
