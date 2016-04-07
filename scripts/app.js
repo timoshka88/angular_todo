46f9c5ca9c0f1dee29239bb854771fb1f337621d
Fatima Khalilova <fatimakhalilova@gmail.com>
2016-04-07 19:11:22 +0200

    created a getTodos function in the service in order to get the Todos from the server

--- scripts/app.js
+++ scripts/app.js
@@ -1,12 +1,21 @@
 angular.module("todoListApp", [])
 .controller("mainCtrl", function($scope, dataService){ //scope - area of work of the controller
 	//adding dataService as a param we create the dependancy as needed in order for the service to be called
-	$scope.helloConsole = dataService.helloConsole;
+	// $scope.helloConsole = dataService.helloConsole;
 	$scope.helloWorld = function(){
 		console.log("Hello there!!!!! I'm the Main Ctrl")
 	};
+	dataService.getTodos(function(response){
+		console.log(response.data);
+		$scope.todos = response.data;
+	});
 
 })
-.service('dataService', function(){
-	this.helloConsole = function(){ console.log("Hello Console this is Setvice")}
+.service('dataService', function($http){
+	this.helloConsole = function(){ console.log("Hello Console this is Setvice")};
+
+	this.getTodos = function(callback){
+		$http.get('mock/todos.json') //takes the url as a first param
+		.then(callback)
+	} 
 });
