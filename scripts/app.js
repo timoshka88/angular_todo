09b29572724204cfe9e4539ee83404fba99f9939
Fatima Khalilova <fatimakhalilova@gmail.com>
2016-04-07 15:46:01 +0200

    created a todos array in app.js and called it from the html to make sure it works

--- scripts/app.js
+++ scripts/app.js
@@ -2,5 +2,12 @@ angular.module("todoListApp", [])
 .controller("mainCtrl", function($scope){ //scope - area of work of the controller
 	$scope.helloWorld = function(){
 		console.log("Hello there!!!!! I'm the Main Ctrl")
-	}
+	};
+
+	$scope.todos = [
+		{"name" : "clean the house"},
+		{"name" : "call XYZ"},
+		{"name" : "enjoy the sun"},
+		{"name" : "enjoy the sun again while it's not raininf"}
+	]
 });
